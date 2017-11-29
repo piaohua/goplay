03b51ff47a86d2dda3d86c5a83b0ba5265a34774
piaohua <twopiao7@gmail.com>
2017-11-30 00:28:06 +0800

    fix mail

--- game/handler/mail.go
+++ game/handler/mail.go
@@ -1,18 +1,14 @@
 package handle
 
 import (
-	"niu/data"
-	"niu/errorcode"
-	"niu/inter"
+	"goplay/data"
+	"goplay/pb"
 	"niu/mail"
-	"niu/protocol"
 	"utils"
-
-	"github.com/golang/protobuf/proto"
 )
 
 //test
-func newMail(p inter.IPlayer) {
+func newMail(p *data.User) {
 	from := "系统消息"
 	content := `北京时间10月19日凌晨，时隔20余月，
 	AlphaGo再次登上科学杂志《nature》。
@@ -24,68 +20,65 @@ func newMail(p inter.IPlayer) {
 	成为全世界最厉害的（人工智能）围棋手。`
 	m := mail.New2(from, p.GetUserid(), content)
 	m.Save()
-	msg := new(protocol.SMailNotice)
+	msg := new(pb.SMailNotice)
 	p.Send(msg)
 }
 
 // 新邮件列表
-func getMailList(ctos *protocol.CMailList, p inter.IPlayer) {
-	stoc := new(protocol.SMailList)
+func getMailList(ctos *pb.CMailList, p *data.User) (stoc *pb.SMailList) {
+	stoc = new(pb.SMailList)
 	maxid := ctos.GetMaxid()
 	list := data.GetNewMailList(maxid, p.GetUserid())
 	for _, v := range list {
-		l := &protocol.MailList{
-			Id:      proto.String(v.Id),
-			From:    proto.String(v.From),
-			Content: proto.String(v.Content),
-			Status:  proto.Uint32(uint32(v.Status)),
-			Etime:   proto.Uint32(uint32(utils.Time2Stamp(v.Etime.Local()))),
-			Ctime:   proto.Uint32(uint32(utils.Time2Stamp(v.Ctime.Local()))),
+		l := &pb.MailList{
+			Id:      v.Id,
+			From:    v.From,
+			Content: v.Content,
+			Status:  uint32(v.Status),
+			Etime:   uint32(utils.Time2Stamp(v.Etime.Local())),
+			Ctime:   uint32(utils.Time2Stamp(v.Ctime.Local())),
 		}
 		for _, val := range v.Attachment {
-			i := &protocol.Items{
-				Rtype:  proto.Uint32(uint32(val.Rtype)),
-				Number: proto.Uint32(val.Number),
+			i := &pb.Items{
+				Rtype:  uint32(val.Rtype),
+				Number: val.Number,
 			}
 			l.Attachment = append(l.Attachment, i)
 		}
 		stoc.List = append(stoc.List, l)
 	}
-	p.Send(stoc)
+	return
 }
 
 // 删除邮件
-func delMail(ctos *protocol.CDeleteMail, p inter.IPlayer) {
-	stoc := new(protocol.SDeleteMail)
+func delMail(ctos *pb.CDeleteMail, p *data.User) (stoc *pb.SDeleteMail) {
+	stoc = new(pb.SDeleteMail)
 	d := new(data.Mail)
 	d.Id = ctos.GetId()
 	d.Del()
-	stoc.Id = proto.String(ctos.GetId())
-	p.Send(stoc)
+	stoc.Id = ctos.GetId()
+	return
 }
 
 // 收取附件
-func getMailItem(ctos *protocol.CGetMailItem, p inter.IPlayer) {
-	stoc := new(protocol.SGetMailItem)
+func getMailItem(ctos *pb.CGetMailItem, p *data.User) (stoc *pb.SGetMailItem) {
+	stoc = new(pb.SGetMailItem)
 	d := new(data.Mail)
 	d.Id = ctos.GetId()
 	d.Get()
 	//没有附件
 	if len(d.Attachment) == 0 {
-		stoc.Error = proto.Uint32(errorcode.NotInRoom)
-		p.Send(stoc)
+		stoc.Error = pb.NotInRoom
 		return
 	}
 	//已经领取过
 	if d.Status == 2 {
-		stoc.Error = proto.Uint32(errorcode.NotInRoom)
-		p.Send(stoc)
+		stoc.Error = pb.NotInRoom
 		return
 	}
 	//已经过期
 	if d.Etime.Before(utils.LocalTime()) { //是否当前时间之前
-		stoc.Error = proto.Uint32(errorcode.NotInRoom)
-		p.Send(stoc)
+		stoc.Error = pb.NotInRoom
 		return
 	}
 	for _, v := range d.Attachment {
@@ -93,6 +86,6 @@ func getMailItem(ctos *protocol.CGetMailItem, p inter.IPlayer) {
 	}
 	d.Status = 2
 	d.Update()
-	stoc.Id = proto.String(ctos.GetId())
-	p.Send(stoc)
+	stoc.Id = ctos.GetId()
+	return
 }
