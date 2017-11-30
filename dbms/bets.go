ed496e1feea353894aecfb9fcbc0d98f13472268
piaohua <twopiao7@gmail.com>
2017-12-01 00:51:16 +0800

    add betting

---
+++ dbms/bets.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"goplay/data"
+	"goplay/glog"
+	"goplay/pb"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/gogo/protobuf/proto"
+)
+
+var (
+	betsPid *actor.PID
+)
+
+//疯狂投注列表服务
+type BetsActor struct {
+	Name string
+	//大厅服务
+	hallPid *actor.PID
+	//唯一id生成
+	uniqueid *data.IDGen
+}
+
+func (a *BetsActor) Receive(ctx actor.Context) {
+	switch msg := ctx.Message().(type) {
+	case *pb.Request:
+		ctx.Respond(&pb.Response{})
+	case *actor.Started:
+		glog.Notice("Starting, initialize actor here")
+	case *actor.Stopping:
+		glog.Notice("Stopping, actor is about to shut down")
+	case *actor.Stopped:
+		glog.Notice("Stopped, actor and its children are stopped")
+	case *actor.Restarting:
+		glog.Notice("Restarting, actor is about to restart")
+	case *actor.ReceiveTimeout:
+		glog.Infof("ReceiveTimeout: %v", ctx.Self().String())
+	case proto.Message:
+		a.Handler(msg, ctx)
+	default:
+		glog.Errorf("unknown message %v", msg)
+	}
+}
+
+func newBetsActor() actor.Actor {
+	a := new(BetsActor)
+	a.Name = cfg.Section("bets").Name()
+	//唯一id初始化
+	a.uniqueid = data.InitIDGen(data.BETTING_KEY)
+	return a
+}
---
+++ dbms/bets_msg.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"goplay/glog"
+	"goplay/pb"
+	"time"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/remote"
+)
+
+func (a *BetsActor) Handler(msg interface{}, ctx actor.Context) {
+	switch msg.(type) {
+	case *pb.HallConnect:
+		//初始化建立连接
+		glog.Infof("bets init: %v", ctx.Self().String())
+		//连接
+		bind := cfg.Section("hall").Key("bind").Value()
+		name := cfg.Section("cookie").Key("name").Value()
+		timeout := 3 * time.Second
+		hallPid, err := remote.SpawnNamed(bind, a.Name, name, timeout)
+		glog.Infof("a.hallPid: %s", a.hallPid.String())
+		if err != nil {
+			glog.Fatalf("remote hall err %v", err)
+		}
+		a.hallPid = hallPid.Pid
+		connect := &pb.HallConnect{
+			Sender: ctx.Self(),
+			Name:   a.Name,
+		}
+		a.hallPid.Tell(connect)
+	case *pb.ServeStop:
+		//关闭服务
+		a.HandlerStop(ctx)
+		//响应登录
+		rsp := new(pb.ServeStoped)
+		ctx.Respond(rsp)
+	default:
+		glog.Errorf("unknown message %v", msg)
+	}
+}
+
+func (a *BetsActor) HandlerStop(ctx actor.Context) {
+	glog.Debugf("HandlerStop: %s", a.Name)
+	//回存数据
+	if a.uniqueid != nil {
+		a.uniqueid.Save()
+	}
+	for k, _ := range a.bets {
+		glog.Debugf("Stop bets: %s", k)
+		//TODO
+		//v.Save()
+	}
+}
