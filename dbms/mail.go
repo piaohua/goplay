27a44434e983d5ac60096fba083daee01e90952f
piaohua <twopiao7@gmail.com>
2017-12-01 00:31:38 +0800

    add mail

---
+++ dbms/mail.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"goplay/data"
+	"goplay/glog"
+	"goplay/pb"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/gogo/protobuf/proto"
+)
+
+var (
+	mailPid *actor.PID
+)
+
+//房间列表服务
+type MailActor struct {
+	Name string
+	//大厅服务
+	hallPid *actor.PID
+	//房间列表
+	mails map[string]*data.Mail
+	//唯一id生成
+	uniqueid *data.IDGen
+}
+
+func (a *MailActor) Receive(ctx actor.Context) {
+	switch msg := ctx.Message().(type) {
+	case *pb.Request:
+		ctx.Respond(&pb.Response{})
+	case *actor.Started:
+		glog.Notice("Starting, initialize actor here")
+	case *actor.Stopping:
+		glog.Notice("Stopping, actor is about to shut down")
+	case *actor.Stopped:
+		glog.Notice("Stopped, actor and its children are stopped")
+	case *actor.Restarting:
+		glog.Notice("Restarting, actor is about to restart")
+	case *actor.ReceiveTimeout:
+		glog.Infof("ReceiveTimeout: %v", ctx.Self().String())
+	case proto.Message:
+		a.Handler(msg, ctx)
+	default:
+		glog.Errorf("unknown message %v", msg)
+	}
+}
+
+func newMailActor() actor.Actor {
+	a := new(MailActor)
+	a.Name = cfg.Section("mail").Name()
+	a.mails = make(map[string]*data.Mail)
+	//唯一id初始化
+	a.uniqueid = data.InitIDGen(data.MAILID_KEY)
+	return a
+}
